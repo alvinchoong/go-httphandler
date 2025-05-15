1f6820ea6dbd8d6597ef059d719f24c9746f103f
Alvin Choong <45955602+alvinchoong@users.noreply.github.com>
2025-05-15 21:49:45 +0800

    make error responder generic to support custom error data types (#5)

--- jsonresp/error.go
+++ jsonresp/error.go
@@ -7,40 +7,40 @@ import (
 )
 
 // Ensure errorResponder implements Responder.
-var _ httphandler.Responder = (*errorResponder)(nil)
+var _ httphandler.Responder = (*errorResponder[any])(nil)
 
 // Error creates a standardized error response with the specified error message and HTTP status code.
 // The 'err' parameter can be used for internal logging.
-func Error(err error, message string, code int) *errorResponder {
-	return &errorResponder{
+func Error[T any](err error, errData T, code int) *errorResponder[T] {
+	return &errorResponder[T]{
 		statusCode: code,
-		errMessage: message,
+		errData:    errData,
 		err:        err,
 	}
 }
 
 // InternalServerError creates a standardized internal server error response.
 // The 'err' parameter can be used for internal logging.
-func InternalServerError(err error) *errorResponder {
-	return &errorResponder{
+func InternalServerError(err error) *errorResponder[string] {
+	return &errorResponder[string]{
 		statusCode: http.StatusInternalServerError,
-		errMessage: "Internal Server Error",
+		errData:    "Internal Server Error",
 		err:        err,
 	}
 }
 
 // errorResponder handles error JSON HTTP responses.
-type errorResponder struct {
+type errorResponder[T any] struct {
 	logger     httphandler.Logger
 	header     http.Header
 	statusCode int
 	cookies    []*http.Cookie
-	errMessage string
+	errData    T
 	err        error
 }
 
 // Respond sends the JSON error response with custom headers, cookies, and status code.
-func (res *errorResponder) Respond(w http.ResponseWriter, _ *http.Request) {
+func (res *errorResponder[T]) Respond(w http.ResponseWriter, _ *http.Request) {
 	// Set cookies.
 	for _, cookie := range res.cookies {
 		http.SetCookie(w, cookie)
@@ -54,18 +54,18 @@ func (res *errorResponder) Respond(w http.ResponseWriter, _ *http.Request) {
 	}
 
 	// Write the error JSON response.
-	writeJSON(w, map[string]string{"error": res.errMessage}, res.statusCode, res.logger)
+	writeJSON(w, map[string]T{"error": res.errData}, res.statusCode, res.logger)
 	httphandler.LogRequestError(res.logger, res.err)
 }
 
 // WithLogger sets the logger for the responder.
-func (res *errorResponder) WithLogger(logger httphandler.Logger) *errorResponder {
+func (res *errorResponder[T]) WithLogger(logger httphandler.Logger) *errorResponder[T] {
 	res.logger = logger
 	return res
 }
 
 // WithHeader adds a custom header to the response.
-func (res *errorResponder) WithHeader(key, value string) *errorResponder {
+func (res *errorResponder[T]) WithHeader(key, value string) *errorResponder[T] {
 	if res.header == nil {
 		res.header = http.Header{}
 	}
@@ -74,7 +74,7 @@ func (res *errorResponder) WithHeader(key, value string) *errorResponder {
 }
 
 // WithCookie adds a cookie to the response.
-func (res *errorResponder) WithCookie(cookie *http.Cookie) *errorResponder {
+func (res *errorResponder[T]) WithCookie(cookie *http.Cookie) *errorResponder[T] {
 	res.cookies = append(res.cookies, cookie)
 	return res
 }
